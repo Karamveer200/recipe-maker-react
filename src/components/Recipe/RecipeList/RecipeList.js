48e5bc731a49d3fd587895a6b9c4aabecf6b3c32
SkillerChampion <128652723+SkillerChampion@users.noreply.github.com>
2024-01-21 17:44:51 -0800

    Added No recipe found animation

--- src/components/Recipe/RecipeList/RecipeList.js
+++ src/components/Recipe/RecipeList/RecipeList.js
@@ -2,14 +2,14 @@
 import { useState, useEffect, useContext } from 'react';
 import BodyContainer from '../../Common/BodyContainer/BodyContainer';
 import SideDrawer from '../../Common/SideDrawer/SideDrawer';
-import { useNavigate } from 'react-router-dom';
 import { LocalStorageContext } from '../../../context/LocalStorageContext';
 import { ALL_ROUTES_PATHS } from '../../../utils/routes';
 import { Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import Lottie from 'react-lottie';
+import emptyLottie from '../../../assets/emptyLottie.json';
 
 const RecipeList = () => {
-  const navigate = useNavigate();
   const { recipes } = useContext(LocalStorageContext);
 
   const [isSideModalOpen, setIsSideModalOpen] = useState(false);
@@ -22,6 +22,15 @@ const RecipeList = () => {
     setIsSideModalOpen(true);
   };
 
+  const renderAddNewRecipeButton = () => (
+    <Button
+      variant="contained"
+      startIcon={<AddIcon />}
+      sx={{ height: '42px', margin: 'auto 0', backgroundColor: '#6366f1' }}>
+      Add New Recipe
+    </Button>
+  );
+
   return (
     <>
       <BodyContainer
@@ -29,16 +38,31 @@ const RecipeList = () => {
         whiteHeading
         className="h-screen"
         rootClassName="h-auto"
-        rightHandComponent={
-          <Button
-            variant="contained"
-            startIcon={<AddIcon />}
-            sx={{ height: '42px', margin: 'auto 0' }}>
-            Add New Recipe
-          </Button>
-        }>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 xl:gap-14 pb-[15px]">
-          <div className="grid md:grid-cols-2 md:gap-4 mt-[10px]"></div>
+        rightHandComponent={renderAddNewRecipeButton()}>
+        <div className="flex pb-[250px]">
+          {recipes?.length ? (
+            <div className=""></div>
+          ) : (
+            <div className="w-full border-2 border-indigo-500 px-10 py-8 rounded-md flex flex-col gap-[50px]">
+              <div className="w-fit mx-auto">
+                <Lottie
+                  options={{
+                    loop: true,
+                    autoplay: true,
+                    animationData: emptyLottie,
+                    rendererSettings: {
+                      preserveAspectRatio: 'xMidYMid slice'
+                    }
+                  }}
+                  height={250}
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center gap-[20px]">
+                <p className="text-2xl font-semibold">Sorry, No recipes found...</p>
+                {renderAddNewRecipeButton()}
+              </div>
+            </div>
+          )}
         </div>
       </BodyContainer>
 
